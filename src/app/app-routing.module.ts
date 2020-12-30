37b56b08783ea135d0db1a937388e84a581e8fa4
eduardogb <eduardo.garciab@hotmail.com>
2020-12-30 16:17:37 -0600

    [ADD] New images effects,  preload all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {LayoutComponent} from './layout/layout.component';
 
 const routes: Routes = [
@@ -27,7 +27,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'top'})],
+    imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'top', preloadingStrategy: PreloadAllModules})],
     exports: [RouterModule]
 })
 export class AppRoutingModule {
--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ProjectsRoutingModule } from './projects-routing.module';
-import { DetailComponent } from './components/detail/detail.component';
+import {ProjectsRoutingModule} from './projects-routing.module';
+import {DetailComponent} from './components/detail/detail.component';
+
+import {LazyLoadImageModule} from 'ng-lazyload-image';
 
 
 @NgModule({
   declarations: [DetailComponent],
   imports: [
     CommonModule,
-    ProjectsRoutingModule
+    ProjectsRoutingModule,
+    LazyLoadImageModule
   ]
 })
-export class ProjectsModule { }
+export class ProjectsModule {
+}
