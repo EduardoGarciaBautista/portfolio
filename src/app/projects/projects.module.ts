a6aafbae7e366a387b5c3e28b3d3b3e38fa962ec
eduardogb <eduardo.garciab@hotmail.com>
2020-12-30 16:58:40 -0600

    [DELETE] Lazy library deleted and unused icons

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -4,7 +4,6 @@ import {CommonModule} from '@angular/common';
 import {ProjectsRoutingModule} from './projects-routing.module';
 import {DetailComponent} from './components/detail/detail.component';
 
-import {LazyLoadImageModule} from 'ng-lazyload-image';
 
 
 @NgModule({
@@ -12,7 +11,6 @@ import {LazyLoadImageModule} from 'ng-lazyload-image';
   imports: [
     CommonModule,
     ProjectsRoutingModule,
-    LazyLoadImageModule
   ]
 })
 export class ProjectsModule {
